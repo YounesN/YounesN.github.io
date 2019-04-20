553b8f048ac3ca729cad8c8e70ff368f08f63425
Younes Nejahi <younes@wayne.edu>
2019-04-20 18:32:20 -0400

    updated skills and tools

--- js/script.js
+++ js/script.js
@@ -118,4 +118,31 @@ $( document ).ready(function() {
       });
     } // End if
   });
+
+  var skills = {"C/C++": 95, "CUDA": 90, "Python": 90, "PHP": 85, "Javascript": 80,
+    "OpenMP": 70, "MPI":70};
+  var tools = {"Git": 95, "VS Code": 95, "Jupyter": 85, "Grace": 85, "CLion": 80,
+    "SVN": 70, "Visual Studio": 70, "Code::Blocks": 65, "NVIDIA Nsight": 65};
+
+  jQuery.each(skills, function(i, val) {
+    var el = $("#skills");
+    el.html(el.html() + fillSkill(i, val));
+  });
+
+  jQuery.each(tools, function(i, val) {
+    var el = $("#tools");
+    el.html(el.html() + fillSkill(i, val));
+  });
+
+  function fillSkill(skill, percent) {
+    var str = "<div class=\"col-sm-2\"><h4>";
+    str += skill;
+    str += "</h4></div><div class=\"col-sm-4\"><div class=\"progress custom-progress\">";
+    str += "<div class=\"progress-bar progress-bar-striped bg-success\" role=\"progressbar\" style=\"width: ";
+    str += percent;
+    str += "%\" aria-valuenow=\"";
+    str += percent;
+    str += "\" aria-valuemin=\"0\" aria-valuemax=\"100\"></div></div></div>";
+    return str;
+  }
 });
